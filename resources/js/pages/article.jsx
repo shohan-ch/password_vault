d9d913b51363dce11781e037f959536114ed6c55
shohan-ch <shohan889@yahoo.com>
2023-05-24 00:49:50 +0600

    react router dom package installed

--- resources/js/pages/article.jsx
+++ resources/js/pages/article.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
 import ArticleView from "../components/Article/ArticleView";
 
+// ReactDOM.render(<ArticleView />, document.getElementById("app"));
 ReactDOM.createRoot(document.getElementById("app")).render(<ArticleView />);
